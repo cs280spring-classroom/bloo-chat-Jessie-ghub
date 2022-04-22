9e2cb387760018e343930af25ee916531f3b95cf
Jessie Luo <jluo30@jhu.edu>
2022-04-22 02:25:31 -0400

    enter left

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,52 @@
-document.addEventListener("DOMContentLoaded", (_event) => {
-  // Connect to socket.io
-  const socket = io(); // automatically tries to connect on same port app was served from
-  const username = document.getElementById("uname").innerText;
-  const form = document.getElementById("chatForm");
-  const messages = document.getElementById("messages");
-  const messageToSend = document.getElementById("txt");
-  form.addEventListener("submit", (event) => {
-    socket.emit("message", {
-      user: username,
-      message: messageToSend.value,
-    });
-    messageToSend.value = "";
-    event.preventDefault();
-  });
+document.addEventListener('DOMContentLoaded', (_event) => {
+	// Connect to socket.io
+	const socket = io(); // automatically tries to connect on same port app was served from
+	const username = document.getElementById('uname').innerText;
+	const form = document.getElementById('chatForm');
+	const messages = document.getElementById('messages');
+	const messageToSend = document.getElementById('txt');
+	form.addEventListener('submit', (event) => {
+		socket.emit('message', {
+			user: username,
+			message: messageToSend.value
+		});
+		messageToSend.value = '';
+		event.preventDefault();
+	});
 
-  // append the chat text message
-  socket.on("message", (msg) => {
-    const message = document.createElement("li");
-    message.innerHTML = `<strong>${msg.user}</strong>: ${msg.message}`;
-    messages.appendChild(message);
-  });
+	// append the chat text message
+	socket.on('message', (msg) => {
+		const message = document.createElement('li');
+		message.innerHTML = `<strong>${msg.user}</strong>: ${msg.message}`;
+		messages.appendChild(message);
+	});
+
+	socket.on('hi', (greeting) => {
+		const message = document.createElement('li');
+		message.innerHTML = `${greeting}`;
+		messages.appendChild(message);
+	});
+
+	socket.on('online', (users) => {
+		const message = document.createElement('li');
+		if (users.length === 1) {
+			message.innerHTML = `no users rn`;
+		} else {
+			pps = '';
+			for (i = 0; i < users.length - 1; i++) {
+				pps += users[i];
+				if (i != users.length - 2 && i >= 0) {
+					pps += ', ';
+				}
+			}
+			message.innerHTML = `${pps} in room`;
+		}
+		messages.appendChild(message);
+	});
+
+	socket.on('new', (name) => {
+		const message = document.createElement('li');
+		message.innerHTML = `<strong>${name}</strong> entered group chat`;
+		messages.appendChild(message);
+	});
 });
--- index.js
+++ index.js
@@ -1,35 +1,51 @@
-const debug = require("debug")("bloo-chat");
-const nunjucks = require("nunjucks");
-const express = require("express");
+const debug = require('debug')('bloo-chat');
+const nunjucks = require('nunjucks');
+const express = require('express');
 const app = express();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 const port = process.env.PORT || 7000;
+users = [];
+nunjucks.configure('views', {
+	autoescape: true,
+	express: app
+});
+const map1 = new Map();
+app.use(express.static('assets'));
 
-nunjucks.configure("views", {
-  autoescape: true,
-  express: app,
+app.get('/', (req, res) => {
+	res.render('index.njk', null);
 });
 
-app.use(express.static("assets"));
-
-app.get("/", (req, res) => {
-  res.render("index.njk", null);
+app.get('/chatroom', (req, res) => {
+//	console.log(users);
+	res.render('chatroom.njk', { uname: req.query.uname });
+	io.emit('new', req.query.uname);
+	users.push(req.query.uname);
+ // console.log(users);
 });
 
-app.get("/chatroom", (req, res) => {
-  res.render("chatroom.njk", { uname: req.query.uname });
-});
+io.on('connection', function(socket) {
+	socket.emit('hi', 'Hi! Welcome to the BLOO CHAT!');
+  //console.log(users);
 
-io.on("connection", function (socket) {
-  socket.on("message", (msg) => {
-    debug(`${msg.user}: ${msg.message}`);
-    //Broadcast the message to everyone
-    io.emit("message", msg);
-  });
+  socket.emit('online', users);
+	socket.on('message', (msg) => {
+		debug(`${msg.user}: ${msg.message}`);
+		//Broadcast the message to everyone
+		io.emit('message', msg);
+	});
+	socket.on('disconnect', function() {
+		console.log("left");
+	});
+  // socket.on('leave', (user) => {
+	// 	console.log("left");
+	// 	//Broadcast the message to everyone
+	// });
+  
 });
 
 http.listen(port, () => {
-  console.log(`Express app listening at http://localhost:${port}`);
+	console.log(`Express app listening at http://localhost:${port}`);
 });
