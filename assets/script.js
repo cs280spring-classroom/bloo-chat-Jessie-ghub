38353e44df8eb7cbdc34d79f61247d19131b4a39
Jessie Luo <jluo30@jhu.edu>
2022-04-27 21:33:21 -0400

    badges

--- assets/script.js
+++ assets/script.js
@@ -22,7 +22,15 @@ document.addEventListener('DOMContentLoaded', (_event) => {
 	// append the chat text message
 	socket.on('message', (msg) => {
 		const message = document.createElement('li');
-		message.innerText = `<strong><u>${msg.user}</u></strong> : ${msg.message}`;
+		const badge = document.createElement('span');
+		badge.classList.add('badge');
+		badge.innerText = msg.user;
+		message.appendChild(badge);
+
+		const text = document.createElement('span');
+		text.innerText = msg.message;
+		message.appendChild(text);
+
 		messages.appendChild(message);
 		messages.scrollTop = messages.scrollHeight;
 	});
@@ -30,25 +38,53 @@ document.addEventListener('DOMContentLoaded', (_event) => {
 	// display welcome message
 	function welcome(username) {
 		const message = document.createElement('li');
-		message.innerText = `<strong><u>BlooChatApp</u></strong>  Welcome ${username}!`;
+		const badge = document.createElement('span');
+		badge.classList.add('badge');
+		badge.classList.add('green');
+		badge.innerText = 'BlooChatApp';
+		message.appendChild(badge);
+
+		const text = document.createElement('span');
+		text.innerText = `Welcome ${username}!`;
+		text.classList.add('green');
+		message.appendChild(text);
+
 		messages.appendChild(message);
+		messages.scrollTop = messages.scrollHeight;
 	}
 
 	// display user exit message
 	socket.on('leave chat', (name) => {
 		if (name) {
 			const message = document.createElement('li');
-			message.innerText = `<strong><u>BlooChatApp</u></strong>  ${name} left the chat`;
-			message.style.backgroundColor = 'red';
+			const badge = document.createElement('span');
+			badge.classList.add('red');
+			badge.classList.add('badge');
+			badge.innerText = 'BlooChatApp';
+			message.appendChild(badge);
+
+			const text = document.createElement('span');
+			text.innerText = `${name} left the chat`;
+			text.classList.add('red');
+			message.appendChild(text);
+
 			messages.appendChild(message);
+			messages.scrollTop = messages.scrollHeight;
 		}
 	});
 
 	// show online users
 	socket.on('online', (users) => {
 		const message = document.createElement('li');
+		const badge = document.createElement('span');
+		badge.classList.add('badge');
+		badge.classList.add('green');
+		badge.innerText = 'BlooChatApp';
+		message.appendChild(badge);
+
+		const text = document.createElement('span');
 		if (users.length === 1) {
-			message.innerText = `<strong><u>BlooChatApp</u></strong>  Unfortunately no one is online at the moment`;
+			text.innerText = `Unfortunately no one is online at the moment`;
 		} else {
 			pps = '';
 			for (i = 0; i < users.length; i++) {
@@ -57,17 +93,31 @@ document.addEventListener('DOMContentLoaded', (_event) => {
 					pps += ' ';
 				}
 			}
-			message.innerText = `<strong><u>BlooChatApp</u></strong>  Online users: ${pps}`;
+			text.innerText = `Online users: ${pps}`;
 		}
+		text.classList.add('green');
+		message.appendChild(text);
+
 		messages.appendChild(message);
+		messages.scrollTop = messages.scrollHeight;
 	});
 
 	// display user entrance message
 	socket.on('enter chat', (name) => {
 		const message = document.createElement('li');
-		message.innerText = `<strong><u>BlooChatApp</u></strong>  <strong>${name}</strong> entered group chat`;
-		message.style.backgroundColor = 'green';
+		const badge = document.createElement('span');
+		badge.classList.add('green');
+		badge.classList.add('badge');
+		badge.innerText = 'BlooChatApp';
+		message.appendChild(badge);
+
+		const text = document.createElement('span');
+		text.innerText = ` ${name} entered group chat`;
+		text.classList.add('green');
+		message.appendChild(text);
+
 		messages.appendChild(message);
+		messages.scrollTop = messages.scrollHeight;
 	});
 
 	socket.on('error', () => {
